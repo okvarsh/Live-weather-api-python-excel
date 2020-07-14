9fb14fdebbf6888874253192670ec6bd0f2d15c6
okvarsh <57315334+okvarsh@users.noreply.github.com>
2020-07-14 17:29:56 +0530

    Update weathers.py

--- weathers.py
+++ weathers.py
@@ -7,11 +7,13 @@ wb1=xw.Book()
 wb1.save('weather.xlsx')
 
 ws1=wb1.sheets[0]
-ws1.range('A1').value=['City', '*C', '*F']
+ws1.range('A1').value=['City','Humidity', '*C', '*F','Update']
 
 cities=["Delhi","London","Karnataka","Mumbai"]
+updates=["1","1","1","1"]
 
 ws1.range('A2').options(transpose=True).value= cities
+ws1.range('E2').options(transpose=True).value= updates
 
 apikey="b9415f7599fb9b6a40714f9448bc1f24"
 #this api is temporary, valid till 14th july 2020
@@ -19,26 +21,32 @@ apikey="b9415f7599fb9b6a40714f9448bc1f24"
 def update_temp():
     for i,c in enumerate(cities):
         k=i+2
-        
-        ws1.cells(k,"B").value=None
-        ws1.cells(k,"C").value=None
+        cell="E"+str(k)
+        if ws1.range(cell).value == 1:
+            ws1.cells(k,"B").value=None
+            ws1.cells(k,"C").value=None
+            ws1.cells(k,"D").value=None
 
-        unit="metric"
-        urlc='https://api.openweathermap.org/data/2.5/weather?q='+c+'&units='+unit+'&appid='+apikey
-        unit="imperial"
-        urlf='https://api.openweathermap.org/data/2.5/weather?q='+c+'&units='+unit+'&appid='+apikey
+            unit="metric"
+            urlc='https://api.openweathermap.org/data/2.5/weather?q='+c+'&units='+unit+'&appid='+apikey
+            unit="imperial"
+            urlf='https://api.openweathermap.org/data/2.5/weather?q='+c+'&units='+unit+'&appid='+apikey
 
-        res=requests.get(urlc).json()
-        temp= res['main']['temp']
-        
-        ws1.cells(k, "B").value = temp
-        
-        res=requests.get(urlf).json()
-        temp= res['main']['temp']
-        ws1.cells(k, "C").value = temp
-        
+            res=requests.get(urlc).json()
+            temp1= res['main']['temp']
+            humi = res['main']['humidity']
+            ws1.cells(k, "B").value = humi
+            ws1.cells(k, "C").value = temp1
+
+            res=requests.get(urlf).json()
+            temp2= res['main']['temp']
+            ws1.cells(k, "C").value = temp1
+            ws1.cells(k, "D").value = temp2
+    updates=["1","1","0","1"]
+    ws1.range('E2').options(transpose=True).value= updates
     print("done")
-    time.sleep(30)
+    time.sleep(3)
     print("wake")
+
 while True:
     update_temp()
